f6158e69dd9d927574a2fe71ddb6fb5dbc8060ab
EK <eke00372@gmail.com>
2018-07-20 13:59:20 +0800

    Add files via upload

---
+++ Clickmenu.lua
@@ -0,0 +1,298 @@
+-- [[ Credit ]] --
+-- NeavUI by Neal: https://www.wowinterface.com/downloads/info13981-NeavUI.html#info
+-- ClickMenu by 10leej: https://www.wowinterface.com/downloads/info22660-ClickMenu.html
+
+--  [[ Click Menu ]] --
+
+-- Right Click Menu List
+local menuFrame = CreateFrame("Frame", "MinimapRightClickMenu", UIParent, "UIDropDownMenuTemplate")
+local menuList = {
+	{	-- 標題
+		text = MAINMENU_BUTTON,
+		isTitle = true,
+		notCheckable = true,
+	},
+	{	-- 角色
+		text = CHARACTER_BUTTON,
+		icon = "Interface\\PaperDollInfoFrame\\UI-EquipmentManager-Toggle",
+		func = function()
+			securecall(ToggleCharacter, "PaperDollFrame") 
+		end,
+		notCheckable = true,
+	},
+	{	-- 法術書
+		text = SPELLBOOK_ABILITIES_BUTTON,
+		icon = "Interface\\MINIMAP\\TRACKING\\Class",
+		func = function() 
+			--securecall(ToggleSpellBook, SpellBookFrame)
+			if not SpellBookFrame:IsShown() then
+				ShowUIPanel(SpellBookFrame)
+			else
+				HideUIPanel(SpellBookFrame)
+			end
+		end,
+		notCheckable = true,
+	},
+	{	--天賦
+		text = TALENTS_BUTTON,
+		icon = "Interface\\MINIMAP\\TRACKING\\Ammunition",
+		func = function() 
+			if (not PlayerTalentFrame) then
+				LoadAddOn("Blizzard_TalentUI")
+			end
+			if (not GlyphFrame) then
+				LoadAddOn("Blizzard_GlyphUI")
+			end 
+			securecall(ToggleFrame, PlayerTalentFrame)
+		end,
+		notCheckable = true,
+	},
+	{		-- 成就
+		text = ACHIEVEMENT_BUTTON,
+		icon = "Interface\\ACHIEVEMENTFRAME\\UI-Achievement-Shield",
+		func = function() 
+			securecall(ToggleAchievementFrame) 
+		end,
+		notCheckable = true,
+	},
+	{	-- 地圖與任務日誌
+		text = MAP_AND_QUEST_LOG,	-- OLD: QUESTLOG_BUTTON
+		icon = "Interface\\GossipFrame\\ActiveQuestIcon",
+		func = function() 
+			securecall(ToggleFrame, WorldMapFrame)
+		end,
+		notCheckable = true,
+	},
+	{	-- 公會和社群
+		text = COMMUNITIES,		-- OLD: COMMUNITIES_FRAME_TITLE
+		icon = "Interface\\GossipFrame\\TabardGossipIcon",
+		arg1 = IsInGuild("player"),
+		func = function() 
+			ToggleCommunitiesFrame()
+		end,
+		notCheckable = true,
+	},
+	{	-- 公會
+		text = GUILD,
+		icon = "Interface\\GossipFrame\\TabardGossipIcon",
+		arg1 = IsInGuild("player"),
+		func = function() 
+			if (not GuildFrame) then
+				LoadAddOn("Blizzard_GuildUI")
+			end
+			--GuildFrame_Toggle()
+			securecall(ToggleFrame, GuildFrame)
+		end,
+		notCheckable = true,
+	},
+	{	-- 好友
+		text = SOCIAL_BUTTON,
+		icon = "Interface\\FriendsFrame\\PlusManz-BattleNet",
+		func = function() 
+			securecall(ToggleFriendsFrame, 1) 
+		end,
+		notCheckable = true,
+	},
+	{	--團隊搜尋器
+		text = ADVENTURE_JOURNAL,	-- DUNGEONS_BUTTON
+		icon = "Interface\\MINIMAP\\TRACKING\\None",
+		func = function() 
+			securecall(ToggleLFDParentFrame)	--OR securecall(PVEFrame_ToggleFrame, "GroupFinderFrame")
+		end,
+		notCheckable = true,
+	},
+	{	-- 收藏
+		text = COLLECTIONS, -- OLD: MOUNTS_AND_PETS
+		icon = "Interface\\MINIMAP\\TRACKING\\StableMaster",
+		func = function() 
+			if InCombatLockdown() then
+				print("|cffffff00"..ERR_NOT_IN_COMBAT.."|r") return
+			end
+			securecall(ToggleCollectionsJournal, 1)
+		end,
+		notCheckable = true,
+	},	
+	{	-- 冒險指南
+		text = ADVENTURE_JOURNAL,	-- OLD: ENCOUNTER_JOURNAL
+		icon = "Interface\\MINIMAP\\TRACKING\\Profession",
+		func = function() 
+			securecall(ToggleEncounterJournal)
+		end,
+		notCheckable = true,
+	},
+	{	-- 遊戲商城
+		text = BLIZZARD_STORE,
+		icon = "Interface\\MINIMAP\\TRACKING\\Auctioneer",
+		func = function()
+			if (not StoreFrame) then
+				LoadAddOn("Blizzard_StoreUI")
+			end
+			securecall(ToggleStoreUI)
+		end,
+		notCheckable = true,
+	},
+	{	-- 空行
+		text = "",
+		isTitle = true,
+		notCheckable = true,
+	},
+	{	-- 其他
+		text = OTHER,
+		isTitle = true,
+		notCheckable = true,
+	},
+	{	-- 背包
+		text = BACKPACK_TOOLTIP,
+		icon = "Interface\\MINIMAP\\TRACKING\\Banker",
+		func = function()
+			securecall(ToggleAllBags)
+		end,
+		notCheckable = true,
+	},
+	{	-- 要塞報告
+		text = GARRISON_LANDING_PAGE_TITLE,
+		icon = "Interface\\HELPFRAME\\OpenTicketIcon",
+		func = function()			
+			securecall(ShowGarrisonLandingPage, 2)
+			--HideUIPanel(GarrisonLandingPage)
+			--ShowGarrisonLandingPage(LE_GARRISON_TYPE_6_0)
+		end,
+		notCheckable = true,
+	},
+	{	-- 職業大廳報告
+		text = ORDER_HALL_LANDING_PAGE_TITLE,
+		icon = "Interface\\HELPFRAME\\OpenTicketIcon",
+		func = function()			
+			securecall(ShowGarrisonLandingPage, 3)
+			--HideUIPanel(GarrisonLandingPage)
+			--ShowGarrisonLandingPage(LE_GARRISON_TYPE_6_0)
+		end,
+		notCheckable = true,
+	},
+	{	-- PVP
+		text = PLAYER_V_PLAYER,
+		icon = "Interface\\MINIMAP\\TRACKING\\BattleMaster",
+		func = function() 
+			securecall(TogglePVPUI, 1) 
+		end,
+		notCheckable = true,
+	},
+	{	-- 團隊
+		text = RAID,
+		icon = "Interface\\TARGETINGFRAME\\UI-TargetingFrame-Skull",
+		func = function() 
+			securecall(ToggleFriendsFrame, 4)
+		end,
+		notCheckable = true,
+	},
+	{	-- 客服支援
+		text = GM_EMAIL_NAME,
+		icon = "Interface\\CHATFRAME\\UI-ChatIcon-Blizz",
+		func = function() 
+			securecall(ToggleHelpFrame) 
+		end,
+		notCheckable = true,
+	},
+	{	-- 行事曆
+		text = SLASH_CALENDAR1:gsub("/(.*)","%1"),	-- 沒有合適的global strings，而且也無法通用全語系本地化，只能將就
+		func = function()
+			if not CalendarFrame then 
+				LoadAddOn("Blizzard_Calendar") 
+			end
+			Calendar_Toggle()
+		end,
+		notCheckable = true,
+	},
+	{	-- 區域地圖
+		text = BATTLEFIELD_MINIMAP,
+		colorCode = "|cff999999",
+		func = function()
+			if not BattlefieldMapFrame then 
+				LoadAddOn("Blizzard_BattlefieldMap") 
+			end
+			BattlefieldMapFrame:Toggle()
+		end,
+		notCheckable = true,
+	},
+	{	-- 空行
+		text = "",
+		isTitle = true,
+		notCheckable = true,
+	},
+	{	-- 插件標題
+		text = ADDONS,
+		isTitle = true,
+		notCheckable = true,
+	},
+	{	-- bigwigs
+		text = "BigWigs",
+		func = function()
+			if not IsAddOnLoaded("Bigwigs") then
+				print("尚未啟用Bigwigs")
+			else
+				SlashCmdList["BigWigs"]("BigWigs1")
+			end
+		end,
+		notCheckable = true,
+	},
+	--[[{	-- DBM
+		text = "DBM",
+		func = function()
+			if not IsAddOnLoaded("DBM-Core") then
+				print("尚未啟用DBM")
+			else
+				DBM:LoadGUI()
+			end
+		end,
+		notCheckable = true,
+	},]]--
+	{	-- 重載
+		text = RELOADUI,
+		colorCode = "|cff999999",
+		func = function()
+			ReloadUI()
+		end,
+		notCheckable = true,
+	},
+	--[[{	-- 遊戲選項
+		text = MAINMENU_BUTTON,
+		func = function()
+			securecall(ToggleGameMenu)
+		end,
+		notCheckable = true,
+	},
+	{	-- 登出
+		text = LOGOUT,
+		func = function()
+			Logout()
+		end,
+		notCheckable = true,
+	},
+	{	-- 退出
+		text = EXIT_GAME,
+		func = function()
+			ForceQuit()
+		end,
+		notCheckable = true,
+	},
+	]]--
+}
+
+-- Right Click for Game Menu, Left Click for Track Menu / 右鍵遊戲選單，中鍵追蹤選單
+Minimap:SetScript("OnMouseUp", function(self, button)
+	if (button == "RightButton") then
+		EasyMenu(menuList, menuFrame, self, (Minimap:GetWidth() * .7), -3, "MENU", 3)
+	elseif (button == "MiddleButton") then
+		ToggleDropDownMenu(1, nil, MiniMapTrackingDropDown, self, (Minimap:GetWidth() * .7), -3)
+	else
+		Minimap_OnClick(self)
+	end
+end)
+
+-- avoid taint / 避免taint
+local initialize = CreateFrame("Frame")
+initialize:SetScript("OnEvent", function()
+	ShowUIPanel(SpellBookFrame)
+	HideUIPanel(SpellBookFrame)
+end)
+initialize:RegisterEvent("PLAYER_ENTERING_WORLD")
---
+++ EKMinimap.lua
@@ -0,0 +1,433 @@
+-- [[ Credit ]] --
+
+-- Felix S. , sakaras, ape47, iMinimap by Chiril, ooMinimap by Ooglogput, intMinimap by Int0xMonkey
+
+-- [[ Config ]] --
+
+-- shift+alt按住標題移動，/rm 重置小地圖位置，/ro重置任務列表位置，alt分享ctrl放棄
+local Scale = 1  					-- 縮放/Scale
+local x , y = 10, -20 				-- 座標/Position
+local AnchorPoint = "TOPLEFT" 		-- 錨點/Anchor point 
+local Announce = false  			-- 行事曆有邀請時高亮邊框/show yellow border when get invite
+
+-- [[ style ]] --
+
+-- class color / 職業顏色
+local ClassColor = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[select(2,UnitClass("player"))]
+
+-- Create font Style / 字型
+local function CreateFS(parent, size, justify)
+	local frame = parent:CreateFontString(nil, "OVERLAY") 
+	frame:SetFont(GameFontNormal:GetFont(), 14, "THINOUTLINE")
+	frame:SetShadowColor(0, 0, 0, 0)
+	frame:SetShadowOffset(0, 0)
+	if justify then
+		frame:SetJustifyH(justify)
+	end
+	return frame
+end
+
+-- [[ Core ]] --
+
+local Minimap = Minimap
+
+-- Make A Square / 弄成方型
+function GetMinimapShape()
+	return "SQUARE"
+end
+--Minimap:SetHitRectInsets(left, right, top, bottom)	-- 俺要方的 不要扁的
+Minimap:SetMaskTexture("Interface\\Buttons\\WHITE8x8")
+Minimap:SetSize(160, 160)
+Minimap:SetScale(Scale)
+Minimap:SetFrameStrata("LOW")
+Minimap:SetFrameLevel(3)
+
+Minimap:ClearAllPoints()
+Minimap:SetPoint(AnchorPoint, UIParent, x, y)
+MinimapCluster:ClearAllPoints()
+MinimapCluster:SetAllPoints(Minimap)
+
+-- Shift+Alt+Drag to move / shift+alt按住移動
+Minimap:SetMovable(true)
+Minimap:EnableMouse(true)
+Minimap:RegisterForDrag("RightButton")
+Minimap:SetScript("OnDragStart", function(self)
+	if IsAltKeyDown() then
+		self:StartMoving()
+	end
+end)
+Minimap:SetScript("OnDragStop", function(self)
+	self:StopMovingOrSizing()
+end)
+
+-- Reset place / 重置位置
+Minimap:SetUserPlaced(true)
+SlashCmdList["RESETMINIMAP"] = function() 
+    Minimap:SetUserPlaced(false)
+    ReloadUI()
+end
+SLASH_RESETMINIMAP1 = "/resetminimap"
+SLASH_RESETMINIMAP2 = "/rm"
+
+-- Background / 背景
+local Background = Minimap:CreateTexture(nil, "BACKGROUND")
+Background:SetTexture("Interface\\Buttons\\WHITE8x8")
+Background:SetPoint("TOPLEFT", Minimap , "TOPLEFT", -1, 1)
+Background:SetPoint("BOTTOMRIGHT", Minimap , "BOTTOMRIGHT", 1, -1)
+Background:SetVertexColor(0, 0, 0)
+
+Background.shadow = CreateFrame("Frame", nil, Minimap)
+Background.shadow:SetPoint("TOPLEFT", -5, 5)
+Background.shadow:SetPoint("BOTTOMRIGHT", 5, -5)
+Background.shadow:SetFrameStrata(Minimap:GetFrameStrata())
+Background.shadow:SetFrameLevel(Minimap:GetFrameLevel()-1)
+Background.shadow:SetFrameLevel(0)
+Background.shadow:SetBackdrop({edgeFile = "Interface\\addons\\EKMinimap\\Media\\glow", edgeSize = 5,})
+Background.shadow:SetBackdropBorderColor(0,0,0)
+
+-- 隱藏圓圈
+Minimap:SetArchBlobRingScalar(0)
+Minimap:SetQuestBlobRingScalar(0)
+
+-- Border Announce for Calendar / 行事曆有邀請時變黃
+local CalendarAnnouncer = CreateFrame("Frame")
+CalendarAnnouncer:SetScript("OnEvent", function(self, event, ...)
+	if Announce then
+		if CalendarGetNumPendingInvites() > 0 then
+			Background:SetVertexColor(1, 1, 0)
+		else
+			Background:SetVertexColor(0, 0, 0)
+		end
+	else return end
+end)
+CalendarAnnouncer:RegisterEvent("CALENDAR_UPDATE_PENDING_INVITES")
+CalendarAnnouncer:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+-- Better Place For World State Frame / 戰場資源條
+--[[local function WorldStateFix()
+	WorldStateAlwaysUpFrame:ClearAllPoints()
+	WorldStateAlwaysUpFrame:SetPoint("TOP", "UIParent", "TOP", 0, -10)
+	for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
+		local frame = _G["AlwaysUpFrame" .. i]
+		
+		local text = _G["AlwaysUpFrame" .. i .. "Text"]
+		text:ClearAllPoints()
+		text:SetPoint("CENTER", frame, "CENTER", -i, 0)
+		text:SetFont(GameFontNormal:GetFont(), 14, "OUTLINE")
+		text:SetShadowColor(0, 0, 0, 0)
+		text:SetJustifyH("CENTER")
+		
+		local icon = _G["AlwaysUpFrame" .. i .. "Icon"]
+		icon:ClearAllPoints()
+		icon:SetPoint("RIGHT", text, "LEFT", 12, -8)
+	end
+end
+hooksecurefunc("WorldStateAlwaysUpFrame_Update", WorldStateFix)]]--
+
+--BattlegroundChatFiltersMisin
+-- [[ Hide Script ]] --
+
+-- Hide Clock / 隱藏時鐘
+local ClockFrame = CreateFrame("Frame", nil, UIParent)
+ClockFrame:SetScript("OnEvent", function(self, event, name)
+	 if name == "Blizzard_TimeManager" then
+		  TimeManagerClockButton:Hide()
+		  TimeManagerClockButton:SetScript("OnShow", function(self)
+				TimeManagerClockButton:Hide()
+		  end)
+	 end
+end)
+ClockFrame:RegisterEvent("ADDON_LOADED")
+
+-- Hide All / 隱藏各種
+local HideAll = {
+	"MinimapBorder",
+	"MinimapBorderTop",
+	"MinimapNorthTag",
+	"MiniMapWorldMapButton",
+	"MinimapZoneTextButton",
+	"MinimapZoomIn",
+	"MinimapZoomOut",
+	"GameTimeFrame",
+	"MiniMapTracking",
+	"ZoneTextFrame",
+	"SubZoneTextFrame",
+	"MiniMapChallengeMode",
+	"DurabilityFrame",
+	"VehicleSeatIndicator",
+}
+for i, v in pairs(HideAll) do
+	getglobal(v).Show = function() end
+	getglobal(v):Hide()
+	--getglobal(v):UnregisterAllEvents()
+end
+
+-- [[ Indicator ]] --
+
+-- Queue Button /佇列圖示
+QueueStatusMinimapButton:ClearAllPoints()
+QueueStatusMinimapButton:SetParent(Minimap)
+if AnchorPoint == "TOPLEFT" or AnchorPoint == "BOTTOMLEFT" then
+	QueueStatusMinimapButton:SetPoint("TOPRIGHT", Minimap, 0, 0)
+else
+	QueueStatusMinimapButton:SetPoint("TOPLEFT", Minimap, 0, 0)
+end
+QueueStatusMinimapButtonBorder:Hide()
+QueueStatusMinimapButton:SetFrameLevel(10)
+
+-- Queue Tooltip fix / 滑鼠提示位置調整
+if AnchorPoint == "TOPLEFT" or AnchorPoint == "BOTTOMLEFT" then
+	QueueStatusFrame:ClearAllPoints()
+	QueueStatusFrame:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 282, -10)
+else
+	QueueStatusFrame:ClearAllPoints()
+	QueueStatusFrame:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -282, -10)
+end
+
+-- Mail Frame / 信件提示
+MiniMapMailFrame:ClearAllPoints()
+MiniMapMailFrame:SetParent(Minimap)
+if AnchorPoint == "TOPLEFT" or AnchorPoint == "BOTTOMLEFT" then
+	MiniMapMailFrame:SetPoint("BOTTOMLEFT", Minimap, 0, 0)
+else
+	MiniMapMailFrame:SetPoint("BOTTOMRIGHT", Minimap, 0, 0)
+end
+MiniMapMailBorder:Hide()
+MiniMapMailIcon:SetTexture("Interface\\AddOns\\EKMinimap\\Media\\mail.tga")
+--MiniMapMailIcon:SetTexture("Interface\\HelpFrame\\ReportLagIcon-Mail.blp")
+--MiniMapMailIcon:SetSize(24,24)
+
+-- Garrison Icon / 要塞和職業大廳
+GarrisonLandingPageMinimapButton:ClearAllPoints()
+GarrisonLandingPageMinimapButton:SetParent(Minimap)
+if AnchorPoint == "TOPLEFT" or AnchorPoint == "BOTTOMLEFT" then
+	GarrisonLandingPageMinimapButton:SetPoint("BOTTOMRIGHT", Minimap, -3, 5)
+else
+	GarrisonLandingPageMinimapButton:SetPoint("BOTTOMLEFT", Minimap, 3, 5)
+end
+hooksecurefunc("GarrisonLandingPageMinimapButton_UpdateIcon", function(button)
+	button:GetNormalTexture():SetTexture("Interface\\HelpFrame\\HelpIcon-ReportLag.blp")
+		button:GetPushedTexture():SetTexture("Interface\\HelpFrame\\HelpIcon-ReportLag.blp")
+	end)
+GarrisonLandingPageMinimapButton:SetScale(0.5)
+
+-- Oderhall Mouseover FadeIn / 淡出淡入
+GarrisonLandingPageMinimapButton:SetAlpha(0)
+GarrisonLandingPageMinimapButton:SetScript("OnEnter", function()
+	securecall(UIFrameFadeIn, GarrisonLandingPageMinimapButton, 0.4, 0, 1)
+end)
+GarrisonLandingPageMinimapButton:SetScript("OnLeave", function()
+	securecall(UIFrameFadeOut, GarrisonLandingPageMinimapButton, 0.8, 1, 0)
+end)
+
+-- MiniMapTracking / 小地圖追蹤
+--[[MiniMapTracking:SetParent(Minimap)
+MiniMapTracking:SetScale(0.8)
+MiniMapTracking:ClearAllPoints()
+MiniMapTracking:SetPoint("BOTTOM", GarrisonLandingPageMinimapButton, "TOP", 0, 0)
+MiniMapTrackingButton:SetNormalTexture("Interface\\HelpFrame\\HelpIcon-CharacterStuck.blp")
+--MiniMapTrackingButton:SetHighlightTexture(nil)
+MiniMapTrackingButton:SetPushedTexture(nil)
+MiniMapTrackingBackground:Hide()
+MiniMapTrackingButtonBorder:Hide()
+
+MiniMapTracking:SetAlpha(0)
+MiniMapTrackingButton:SetScript("OnEnter", function()
+	securecall(UIFrameFadeIn, MiniMapTracking, 0.4, 0, 1)
+end)
+MiniMapTrackingButton:SetScript("OnLeave", function()
+	securecall(UIFrameFadeOut, MiniMapTracking, 0.8, 1, 0)
+end)
+]]--
+-- [[ Instance Difficulty ]] -- 
+
+-- Hide Blizzard / 隱藏暴雪的難度旗子
+MiniMapInstanceDifficulty:Hide()
+MiniMapInstanceDifficulty.Show = function() return end
+GuildInstanceDifficulty:Hide()
+GuildInstanceDifficulty.Show = function() return end
+
+-- Creat Frame / 創建框體
+local RaidDifficulty = CreateFrame("Frame", nil, Minimap)
+RaidDifficulty:SetSize(44, 44)
+if AnchorPoint == "TOPLEFT" or AnchorPoint == "BOTTOMLEFT" then
+	RaidDifficulty:SetPoint("TOPLEFT", Minimap,  -5, 5)
+else
+	RaidDifficulty:SetPoint("TOPRIGHT", Minimap, 5, 5)
+end
+RaidDifficulty.Texture = RaidDifficulty:CreateTexture(nil, 'OVERLAY')
+RaidDifficulty.Texture:SetAllPoints(true)
+RaidDifficulty.Texture:SetTexture("Interface\\AddOns\\EKMinimap\\Media\\difficulty.tga")
+RaidDifficulty.Texture:SetAlpha(.8)
+RaidDifficulty.Texture:SetVertexColor(ClassColor.r, ClassColor.g, ClassColor.b)
+RaidDifficulty:Hide()
+RaidDifficulty:RegisterEvent("PLAYER_ENTERING_WORLD")
+RaidDifficulty:RegisterEvent("PLAYER_DIFFICULTY_CHANGED")
+RaidDifficulty:RegisterEvent("GUILD_PARTY_STATE_UPDATED")
+RaidDifficulty:RegisterEvent("INSTANCE_GROUP_SIZE_CHANGED")
+RaidDifficulty:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+RaidDifficulty:RegisterEvent("CHALLENGE_MODE_START")
+RaidDifficulty:RegisterEvent("CHALLENGE_MODE_COMPLETED")
+RaidDifficulty:RegisterEvent("CHALLENGE_MODE_RESET")
+
+-- Difficulty Text / 難度文字
+local RaidDifficultyText = CreateFS(RaidDifficulty, 10, "CENTER")
+RaidDifficultyText:SetPoint("CENTER")
+
+-- Difficulty / 地城難度(與公會團隊)
+RaidDifficulty:SetScript("OnEvent", function()
+	local _, instanceType = IsInInstance()
+	local difficulty = select(3, GetInstanceInfo())
+	local num = select(9, GetInstanceInfo())
+	--local _, instanceType, difficulty, _, _, _, _, _, instanceGroupSize = GetInstanceInfo()
+	local mplus = select(1, C_ChallengeMode.GetActiveKeystoneInfo()) or ""
+	
+	if instanceType == "party" or instanceType == "raid" or instanceType == "scenario" then
+		if difficulty == 1 then
+			RaidDifficultyText:SetText("5N")
+		elseif difficulty == 2 then
+			RaidDifficultyText:SetText("5H")
+		elseif difficulty == 3 then
+			RaidDifficultyText:SetText("10N")
+		elseif difficulty == 4 then
+			RaidDifficultyText:SetText("25N")
+		elseif difficulty == 5 then
+			RaidDifficultyText:SetText("10H")
+		elseif difficulty == 6 then
+			RaidDifficultyText:SetText("25H")
+		elseif difficulty == 7 then
+			RaidDifficultyText:SetText("LFR")
+		elseif difficulty == 8 then
+			RaidDifficultyText:SetText("M"..mplus)	-- Challenge Mode and Mythic+
+		elseif difficulty == 9 then
+			RaidDifficultyText:SetText("40R")
+		elseif difficulty == 11	then
+			RaidDifficultyText:SetText("3H")
+		elseif difficulty == 12	then 
+			RaidDifficultyText:SetText("3N")
+		elseif difficulty == 14	then
+			RaidDifficultyText:SetText(num .. "N")
+		elseif difficulty == 15	then
+			RaidDifficultyText:SetText(num .. "H")
+		elseif difficulty == 16	then
+			RaidDifficultyText:SetText("M")
+		elseif difficulty == 17	then
+			RaidDifficultyText:SetText(num .. "L")
+		elseif difficulty == 18 or difficulty == 19 or difficulty == 20 then
+			RaidDifficultyText:SetText("E")
+		elseif difficulty == 23	then
+			RaidDifficultyText:SetText("5M")
+		elseif difficulty == 24 then
+			RaidDifficultyText:SetText("T")
+		end
+	elseif instanceType == "pvp" or instanceType == "arena" then
+		RaidDifficultyText:SetText("PVP")
+	else
+		RaidDifficultyText:SetText("")
+	end
+	
+	if not IsInInstance() then
+		RaidDifficulty:Hide()
+	else
+		RaidDifficulty:Show()
+	end
+	
+	--[[if GuildInstanceDifficulty:IsShown() then
+		RaidDifficultyText:SetTextColor(0, .9, 0)
+	else
+		RaidDifficultyText:SetTextColor(1, 1, 1)
+	end]]--
+end)
+
+-- [[ Misc ]] --
+
+-- Scroll Zoom, Alt+Scroll Scale / 滾輪縮放區域，alt滾輪縮放大小
+local function OnMouseWheel(self, delta)
+	if IsAltKeyDown() then
+		local i = Minimap:GetScale()
+		 if delta > 0 and i < 4 then
+			Minimap:SetScale(i+0.1)
+		 elseif delta < 0 and i > 0.5 then
+			Minimap:SetScale(i-0.1)
+		end
+	else
+		if delta > 0 then
+			Minimap_ZoomIn()
+		else
+			Minimap_ZoomOut()
+		end
+	end
+end
+Minimap:SetScript("OnMouseWheel", OnMouseWheel)
+
+-- Show minimap ping / 顯示誰點擊了小地圖
+local WhoPing = CreateFrame("Frame", nil, Minimap)
+WhoPing:SetSize(100, 20)
+WhoPing:SetPoint("BOTTOM", Minimap, 0, 2)
+WhoPing:RegisterEvent("MINIMAP_PING")
+
+local WhoPingText = CreateFS(WhoPing, 10, "CENTER")
+WhoPingText:SetPoint("CENTER")
+
+local anim = WhoPing:CreateAnimationGroup()
+anim:SetScript("OnPlay", function()
+	WhoPing:SetAlpha(1)
+end)
+anim:SetScript("OnFinished", function()
+	WhoPing:SetAlpha(0)
+end)
+anim.fader = anim:CreateAnimation("Alpha")
+anim.fader:SetFromAlpha(1)
+anim.fader:SetToAlpha(0)
+anim.fader:SetDuration(4)
+anim.fader:SetSmoothing("OUT")
+anim.fader:SetStartDelay(3)
+
+WhoPing:SetScript("OnEvent", function(_, _, unit)
+	local name = GetUnitName(unit)
+	anim:Stop()
+	WhoPingText:SetText(name)
+	WhoPingText:SetTextColor(ClassColor.r, ClassColor.g, ClassColor.b)
+	anim:Play()
+end)
+
+-- Hide order hall bar: https://github.com/destroyerdust/Class-Hall
+local HideOH = CreateFrame("Frame")
+HideOH:SetScript("OnUpdate", function(self,...)
+	local OrderHallCommandBar = OrderHallCommandBar
+	if OrderHallCommandBar then
+		OrderHallCommandBar:Hide()
+		OrderHallCommandBar:UnregisterAllEvents()
+		OrderHallCommandBar.Show = function() end
+	end
+	OrderHall_CheckCommandBar = function () end
+	self:SetScript("OnUpdate", nil)
+end)
+	
+--right click toggle garrison page
+--[[
+GarrisonLandingPageMinimapButton:SetScript("OnClick", function(self, button, down)
+	if button == "LeftButton" then
+		if (GarrisonLandingPage and GarrisonLandingPage:IsShown()) then
+			HideUIPanel(GarrisonLandingPage)
+		else
+			ShowGarrisonLandingPage(C_Garrison.GetLandingPageGarrisonType())
+		end
+	elseif button == "RightButton" then
+		if (GarrisonLandingPage and GarrisonLandingPage:IsShown()) then
+			HideUIPanel(GarrisonLandingPage)
+		else
+			ShowGarrisonLandingPage(LE_GARRISON_TYPE_6_0)
+		end
+	end
+end)
+GarrisonLandingPageMinimapButton:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+]]--
+
+--[[
+GarrisonLandingPageMinimapButton:HookScript("OnClick", function(self, button, down)
+   if not (button == "RightButton" and not down) then return end
+   HideUIPanel(GarrisonLandingPage)
+   ShowGarrisonLandingPage(LE_GARRISON_TYPE_6_0)
+end)
+GarrisonLandingPageMinimapButton:RegisterForClicks("LeftButtonUp", "RightButtonUp")]]--
