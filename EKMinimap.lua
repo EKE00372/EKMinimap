627a427818e5c90e79211efee7ec59ad48c6c914
EKE00372 <eke00372@gmail.com>
2019-08-31 14:59:19 +0800

    add notes.

--- EKMinimap.lua
+++ EKMinimap.lua
@@ -1,6 +1,8 @@
 local C, G = unpack(select(2, ...))
 
--- [[ style ]] --
+--====================================================--
+-----------------    [[ Function ]]    -----------------
+--====================================================--
 
 -- Create font style / 字型
 local function CreateFS(parent, justify)
@@ -154,24 +156,25 @@ MiniMapTrackingFrame.BG = CreateShadow(MiniMapTrackingFrame, MiniMapTrackingIcon
 --================================================--
 
 local WMF = WorldMapFrame
-	WMF:SetScale(0.6)
-	WMF.BlackoutFrame.Blackout:SetAlpha(0)
+	WMF:SetScale(0.6)							-- Scale / 縮放
+	WMF.BlackoutFrame.Blackout:SetAlpha(0)		-- No background / 去背
 	--WMF.BlackoutFrame.Blackout = function() end
-	WMF.BlackoutFrame:EnableMouse(false)
-	WorldMapFrame.ScrollContainer.GetCursorPosition = function(f)
+	WMF.BlackoutFrame:EnableMouse(false)		-- Click through / 點擊穿透	
+	-- Cursor match scale / 滑鼠跟隨縮放
+	WMF.ScrollContainer.GetCursorPosition = function(f)
 		local x,y = MapCanvasScrollControllerMixin.GetCursorPosition(f)
 		local s = WorldMapFrame:GetScale()
 		
 		return x/s, y/s
 	end
 	-- Movable
-	WMF:SetMovable(true)
+	WMF:SetMovable(true)						-- 使地圖可移動
 	WMF:SetUserPlaced(true)
 	WMF:ClearAllPoints()
 	WMF.ClearAllPoints = function() end			-- 使座標可自訂義
-	WMF:SetPoint("LEFT", UIParent)				-- 畫面左邊
-	--WMF.SetPoint = function() end				-- 使拖動過的位置可以被儲存
-	-- Alt+right click to drag frame
+	WMF:SetPoint("LEFT", UIParent)				-- Default at left / 初始化於畫面左邊
+	--WMF.SetPoint = function() end				-- 使拖動過的位置可以被儲存，不想，事多
+	-- Alt+right click to drag frame / alt+右鍵拖動
 	WMF:RegisterForDrag("RightButton")
 	WMF:SetScript("OnDragStart", function(self)
 		if IsAltKeyDown() then
@@ -188,7 +191,7 @@ local WMF = WorldMapFrame
 	end
 	SLASH_RESETMAP1 = "/resetmap"
 	SLASH_RESETMAP2 = "/rmm"]]--
-	-- Fadeout
+	-- Fadeout when moving / 移動時淡出
 	if event == "PLAYER_STOPPED_MOVING" then
 		PlayerMovementFrameFader.AddDeferredFrame(WMF, 1, 3.0, .5)
 	else
